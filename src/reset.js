672a9954d23e865df6fea6c64c1a04ff518551bc
bearyu99 <ryu.kultra@gmail.com>
2023-05-01 02:21:43 +0900

    리셋 슬라이드 애니메이션 추가

--- src/reset.js
+++ src/reset.js
@@ -7,15 +7,25 @@ icon.addEventListener("mousedown", function () {
 });
 
 icon.addEventListener("mouseup", function () {
+  icon.classList.remove("slide-animation");
+  clearInterval(intervalId);
+  count = 0;
+});
+
+icon.parentElement.addEventListener("mouseleave", function () {
+  icon.classList.remove("slide-animation");
   clearInterval(intervalId);
   count = 0;
 });
 
 function checkCount() {
   count += 1;
-  console.log(count);
+  if (!icon.classList.contains("slide-animation")) {
+    icon.classList.add("slide-animation");
+  }
   if (count >= 5) {
     const reset = confirm("초기화하시겠습니까?");
+    icon.classList.remove("slide-animation");
     if (reset) {
       localStorage.clear();
       location.reload();
